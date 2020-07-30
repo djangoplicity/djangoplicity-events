b892a2b1d14d12369a1a043d719b4064396c9549
Edison Arango <edisonarab@gmail.com>
2020-07-29 22:13:50 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ from setuptools import setup, find_packages
 setup(
     name='djangoplicity-events',
     version='0.1.0',
-    packages=find_packages(include=['djangoplicity.*'],
+    packages=find_packages(include=['djangoplicity.*']),
     include_package_data=True,
     zip_safe=False,
     install_requires=[

079b8f169de83d66d6ae6d8804327f0a4b4c04f4
Edison Arango <edisonarab@gmail.com>
2020-09-10 17:45:57 -0500

    Configure default URL

--- test_project/urls.py
+++ test_project/urls.py
@@ -16,12 +16,15 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
+from djangoplicity.events.models import Event
+from djangoplicity.events.options import EventOptions
 
 urlpatterns = [
     # Djangoplicity Administration
     url(r'^admin/', admin.site.urls),
     url(r'^admin/import/', include('djangoplicity.archives.importer.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
+    url(r'^$', include('djangoplicity.events.urls'), {'model': Event, 'options': EventOptions, 'translate': False}),
 ]
 
 # This only works if DEBUG=True
